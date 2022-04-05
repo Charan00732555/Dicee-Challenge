d536d95215a7cad3e6d4f85245bd3aa54a0c0758
Charantej Komaram <86775300+Charan00732555@users.noreply.github.com>
2022-04-05 20:14:45 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,94 +1,79 @@
+var level=0;
+var started = false;
 
-var buttonColours = ["red", "blue", "green", "yellow"];
 
-var gamePattern = [];
+var buttonColours= ["red", "blue", "green", "yellow"];
+var gamePattern=[];
 var userClickedPattern = [];
 
-var started = false;
-var level = 0;
+function playsound(name){
+    var audio=new Audio(name+".mp3");
+    audio.play();
+}
 
-$(document).keypress(function() {
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
-});
-
-$(".btn").click(function() {
-
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-
-  checkAnswer(userClickedPattern.length-1);
-});
-
-
-function checkAnswer(currentLevel) {
-
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
-      console.log("success");
-
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-
-    } else {
-
-      console.log("wrong");
-
-      playSound("wrong");
-
-      $("body").addClass("game-over");
-      setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-
-      $("#level-title").text("Game Over, Press Any Key to Restart");
-
-      //2. Call startOver() if the user gets the sequence wrong.
-      startOver();
+$(".main1").click(function(){
+    if(!started){
+        $("h1").text("Level "+level);
+        newSequence();
+        started=true;
     }
+});
 
+$(".btn").click(function(){
+   var userChosenColour= $(this).attr("id");
+   userClickedPattern.push(userChosenColour);
+   playsound(userChosenColour);   
+   animatePress(userChosenColour);
+   checkAnswer(userClickedPattern.length-1);
+});
+
+function newSequence(){
+    userClickedPattern=[];
+    level++;
+    $("h1").text("Level-"+level);
+
+    var randomnumber=Math.floor(Math.random()*4);
+    var randomChosenColour= buttonColours[randomnumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playsound(randomChosenColour);
 }
 
-function nextSequence() {
-
-  userClickedPattern = [];
-  level++;
-  $("#level-title").text("Level " + level);
-
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColour);
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed");
+    },200);
 }
 
-function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
+function checkAnswer(currentlevel){
+     if(gamePattern[currentlevel]===userClickedPattern[currentlevel]){
+         if(userClickedPattern.length===gamePattern.length){
+             setTimeout(function(){
+                 newSequence();
+             },1000);
+         }
+     }else{
+         playsound("wrong");
+         $("body").addClass("game-over")
+         setTimeout(function(){
+          $("body").removeClass("game-over")
+         },200);
+         $("h1").text("Game Over, Press Below Key to Restart");
+         startOver();
+     }
 }
 
-function animatePress(currentColor) {
-  $("#" + currentColor).addClass("pressed");
-  setTimeout(function () {
-    $("#" + currentColor).removeClass("pressed");
-  }, 100);
+function startOver(){
+    level=0;
+    gamePattern=[];
+    started=false;
 }
 
-//1. Create a new function called startOver().
-function startOver() {
 
-  //3. Inside this function, you'll need to reset the values of level, gamePattern and started variables.
-  level = 0;
-  gamePattern = [];
-  started = false;
-}
+
+
+
+
+  
+    
